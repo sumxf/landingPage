755d59000159d13724861b9649eeec90bf5c0733
iffmainak <mainakd@gmail.com>
2017-10-29 13:48:02 +0000

    adding umbotron

--- newIndex.php
+++ newIndex.php
@@ -4,11 +4,16 @@
   <title>Sumxf - Automatic analytics for all!</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 
+<div class="jumbotron">
 <h1>Sumxf</h1> 
 <p>We specialize in analytics </p> 
+</div>
 
 </body>
 </html>
