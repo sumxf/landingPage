52ed04174c76d4257cc337bb4c099d0327327eb5
iffmainak <mainakd@gmail.com>
2017-10-29 13:30:26 +0000

    starting new landing page from scratch

---
+++ newIndex.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Sumxf - Automatic analytics for all!</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<h1>Sumxf</h1> 
+<p>We specialize in analytics </p> 
+
+</body>
+</html>
