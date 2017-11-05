58bc9b33152135016252cb3ab63970a43b584574
iffmainak <mainakd@gmail.com>
2017-11-05 01:42:33 +0000

    added badge

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 </style>
 
 <div class="jumbotron text-center">
-<h1>ERJS</h1> <span class="label label-warning">beta</span> 
+<h1>ERJS</h1> <span class="badge">beta</span> 
 <p>Machine Learning. By Anyone. For Anyone. </p> 
 </div>
 
