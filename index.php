89ee469e467fc8c48ec29e86e57ba362ff946176
iffmainak <mainakd@gmail.com>
2017-11-05 02:03:34 +0000

    added beta warning

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 </style>
 
 <div class="jumbotron text-center">
-<h1 style="display:inline;">ERJS</h1> <span class="badge">beta</span>
+<h1 style="display:inline;">ERJS</h1> <span class="label label-warning">beta</span>
 <p>Machine Learning. By Anyone. For Anyone. </p> 
 </div>
 
