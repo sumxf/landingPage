d8b9e88cd322d2ab1c14e10a8cc391cb35b7d0e2
iffmainak <mainakd@gmail.com>
2017-11-06 05:56:06 +0000

    footer

--- index.php
+++ index.php
@@ -29,7 +29,13 @@
     background-color: #f6f6f6;
 }
 </style>
-
+<style>
+footer .glyphicon {
+    font-size: 20px;
+    margin-bottom: 20px;
+    color: #f4511e;
+}
+</style>
 <style>
   .navbar {
     margin-bottom: 0;
@@ -258,7 +264,13 @@
     </div>
   </div>
 </nav>
-  
+
+<footer class="container-fluid text-center">
+  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" title="Visit w3schools">www.w3schools.com</a></p> 
+</footer>
 </div>
 
 </body>
