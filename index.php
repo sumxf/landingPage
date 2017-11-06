7197f50d4f4123e88a6403f4c0c70547a6dee613
iffmainak <mainakd@gmail.com>
2017-11-06 06:01:18 +0000

    corrected body id

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body id="mypage">
+<body id="myPage">
 
 <style>
 .jumbotron { 
