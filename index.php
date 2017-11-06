9c1e2da63a28c8cf45287accbac8b09743ebe2da
iffmainak <mainakd@gmail.com>
2017-11-06 05:32:10 +0000

    reverting back

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+<body>
 
 <style>
 .jumbotron { 
@@ -57,13 +57,6 @@
 }
 
 </style>
-
-
-
-<div id="about" class="container-fluid">
-<div id="capabilities" class="container-fluid">
-<div id="login-signup" class="container-fluid">
-<div id="contact" class="container-fluid">
 
 <style>
 .logo {
