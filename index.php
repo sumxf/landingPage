b6a158a04269211ff7f452de93a629ecd379f0e5
iffmainak <mainakd@gmail.com>
2017-12-06 01:44:23 +0000

    correcting the link

--- index.php
+++ index.php
@@ -122,7 +122,7 @@ footer .glyphicon {
     <div class = "col-sm-6">
       <br><br><br><br>
        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <a href = "https://login.shinyapps.io/login?redirect=%2Foauth%2Fauthorize%3Fshow_auth%3D0%26state%3D%26redirect_uri%3Dhttps%253A%252F%252Ferjs.shinyapps.io%252FsumxfNew01%252F__login__%252F%26response_type%3Dcode%26show_login%3D0%26client_application%3D95661%26client_signature%3D7924e37e2d26da03d8c5147b51dff55307d05607760bce00038aab8ff2ae0946%26client_id%3DE87881F52601DE697EF13011621DDDE6713C44968B8D2782" 
+      <a href = "https://login.shinyapps.io/login?redirect=%2Foauth%2Fauthorize%3Fshow_auth%3D0%26state%3D%26redirect_uri%3Dhttps%253A%252F%252Ferjs.shinyapps.io%252Fsumxfnew01%252F__login__%252F%26response_type%3Dcode%26show_login%3D0%26client_application%3D244524%26client_signature%3Daf15fa0f3d05b14ac888469475dbabedaed6873ef2003f355e605e0dfdefb444%26client_id%3DD6A344AB5B2FA6EC85D3D27F9FBE712996523F0C9C0258A7" 
          class = "btn btn-primary"> Login </a>
          <br><br><br>
       <!-- img src = "Logo1.jpg" class = "img-rounded" alt = "SUMXF" -->
