914044244a5d810c82ed1e33bdcdcc5a6cce3808
iffmainak <mainakd@gmail.com>
2018-02-09 04:58:43 +0000

    changed to alpha

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ footer .glyphicon {
 </style>
 
 <div class="jumbotron text-center">
-<h1 style="display:inline;">ERJS</h1> <span class="label label-warning">beta</span>
+<h1 style="display:inline;">ERJS</h1> <span class="label label-warning">alpha</span>
 <p>Machine Learning. By Anyone. For Anyone. </p> 
 </div>
 
