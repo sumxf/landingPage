da533e3c0b3397165b29e9a950292a32fb1b3cfd
iffmainak <mainakd@gmail.com>
2018-02-20 22:48:39 +0000

    some change before email

--- send_mail.php
+++ send_mail.php
@@ -4,13 +4,13 @@ require_once 'swiftmailer/lib/swift_required.php';
 // ...
 
 // make sure you get these SMTP settings right
-$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl") 
+$transport = \Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl") 
     ->setUsername('erjssumxf@gmail.com')
     ->setPassword('HWVer1BIBA');
 
-$mailer = Swift_Mailer::newInstance($transport);
+$mailer = \Swift_Mailer::newInstance($transport);
 // the message itself
-$message = Swift_Message::newInstance('email subject')
+$message = \Swift_Message::newInstance('email subject')
     ->setFrom(array('erjssumxf@gmail.com' => 'no reply'))
     ->setTo(array('support@erjs.io'))
     ->setBody("email body");
